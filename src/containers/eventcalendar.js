c0270552fb6e0250536b9ba0313332041d59a4c9
Anna Baron <abaron@live.com>
2018-01-27 15:26:12 -0800

    Attempt to add events to calendar

--- src/containers/eventcalendar.js
+++ src/containers/eventcalendar.js
@@ -4,18 +4,23 @@ import actions from '../actions/index';
 import moment from 'moment';
 
 import BigCalendar from 'react-big-calendar';
+import events from '../data/events';
 
 BigCalendar.momentLocalizer(moment)
 
 require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css');
 
+console.log(events);
 class EventCalendar extends React.Component {
   render() {
     return (
       <div>
         <BigCalendar
           style={{height: '420px'}}
-          events={[]}
+          views ={['month']}
+          events={events}
+          startAccessor="startDate"
+          endAccessor="endDate"
         />
       </div>
     )
---
+++ src/data/events.js
@@ -0,0 +1,14 @@
+export default [
+{
+  allDay: false,
+  endDate: new Date('December 10, 2017 11:13:00'),
+  startDate: new Date('December 09, 2017 11:13:00'),
+  title: 'hi',
+},
+{
+  allDay: true,
+  endDate: new Date('December 09, 2017 11:13:00'),
+  startDate: new Date('December 09, 2017 11:13:00'),
+  title: 'All Day Event',
+},
+];
