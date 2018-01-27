b68e2c278bbf72adcc6b6bf7c19715e4829f7e25
Eduardo Muramoto <dudmuramoto@gmail.com>
2018-01-27 13:58:02 -0800

    add wrapped container

--- src/containers/directory.js
+++ src/containers/directory.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../actions/index';
-import MapContainer from './maps'
+import WrappedContainer from './maps'
 
 class Directory extends React.Component {
   render() {
     return (
       <div className={this.props.directoryOpen ? "directory-open" : "directory-closed"}>
         <h1>Directory</h1>
-        <MapContainer />
+        <WrappedContainer />
       </div>
     )
   }
--- src/containers/maps.js
+++ src/containers/maps.js
@@ -1,5 +1,6 @@
 
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class MapContainer extends React.Component {
@@ -7,6 +8,7 @@ class MapContainer extends React.Component {
     return (
       <div className="MapContainer">
       <Map
+        style={{width: '50%', height: '50%'}}
         google={this.props.google}
         initialCenter={{
             lat: 49.2819605, lng: -123.1086604
@@ -18,7 +20,7 @@ class MapContainer extends React.Component {
           name={'Code Core'}
           position={{lat: 49.2819605, lng: -123.1086604}} />
         <Marker
-          name={'Hoot'}
+          name={'Code Core'}
           position={{lat: 49.26382, lng: -123.104321}} />
 
 
@@ -28,6 +30,9 @@ class MapContainer extends React.Component {
     }
   }
 
-  export default GoogleApiWrapper({
+
+const WrappedContainer = (GoogleApiWrapper({
     apiKey: "AIzaSyCHgbwaK-pbR1UPZGuXge3dhwQTSYCVzv0"
-  })(MapContainer)
+  })(MapContainer))
+
+  export default WrappedContainer
