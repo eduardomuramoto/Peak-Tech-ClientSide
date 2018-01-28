19012b6f052dd137d6c298c27b49daeb7ee7f7bf
Anna Baron <33434263+annajbaron@users.noreply.github.com>
2018-01-27 16:53:16 -0800

    Event calendar (#4)
    * Add event calendar component, add antd package
    
    * Display calendar in event component
    
    * Attempt to add events to calendar
    
    * Add events to calendar

---
+++ src/containers/eventcalendar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import actions from '../actions/index';
+import moment from 'moment';
+
+import BigCalendar from 'react-big-calendar';
+import events from '../data/events';
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
+// BigCalendar.momentLocalizer(moment)
+
+require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css');
+
+console.log(events);
+class EventCalendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <BigCalendar
+          style={{height: '420px'}}
+          views ={['month']}
+          events={events}
+        />
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventCalendar);
