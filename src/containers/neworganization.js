59710e16502f4b1a1f2e80559efd0307a3ffc133
Eduardo Muramoto <33434262+eduardomuramoto@users.noreply.github.com>
2018-01-28 09:43:01 -0800

    Add handle submit to new organization and onInput (#10)

--- src/containers/neworganization.js
+++ src/containers/neworganization.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../actions/index';
+import {Organization} from '../requests/organizations.js';
 
 class NewOrganization extends React.Component {
   constructor(){
@@ -25,7 +26,11 @@ class NewOrganization extends React.Component {
   }
 
   handleSubmit(){
-
+    Organization
+      .create({organization: this.state})
+      .then(data => {
+        console.log(data)
+      })
   }
 
   render() {
@@ -37,49 +42,49 @@ class NewOrganization extends React.Component {
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" id="input_email" name="name" className="form-control" placeholder="Name" onChange={this.handleChange.bind(this)} value={this.state.email}/>
+              <input type="text" id="input_name" name="name" className="form-control" placeholder="Name" onInput={this.handleChange.bind(this)} value={this.state.email}/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="address" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Address"/>
+              <input type="text" className="form-control" name="address" id="input_address" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Address"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="overview" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Overview"/>
+              <input type="text" className="form-control" name="overview" id="input_overview" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Overview"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="employees" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Employees"/>
+              <input type="text" className="form-control" name="employees" id="input_employees" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Employees"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="team_size" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Team Size"/>
+              <input type="number" className="form-control" name="team_size" id="input_team_size" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Team Size"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="website" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Website"/>
+              <input type="text" className="form-control" name="website" id="input_website" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Website"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="twitter" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Twitter Handle"/>
+              <input type="text" className="form-control" name="twitter" id="input_twitter" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Twitter Handle"/>
             </div>
           </div>
 
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control" name="logo" id="input_last_name" onChange={this.handleChange.bind(this)} value={this.state.password} placeholder="Logo Url"/>
+              <input type="text" className="form-control" name="logo" id="input_logo" onInput={this.handleChange.bind(this)} value={this.state.password} placeholder="Logo Url"/>
             </div>
           </div>
 
