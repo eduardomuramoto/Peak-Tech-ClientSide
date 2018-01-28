840a559a033fca4d08facfbce464e714b0398e30
Eduardo Muramoto <dudmuramoto@gmail.com>
2018-01-27 18:15:43 -0800

    Add the infoshow to maps

--- src/containers/maps.js
+++ src/containers/maps.js
@@ -3,11 +3,31 @@ import {connect} from 'react-redux';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+  }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
   render() {
     return (
       <div className="MapContainer">
       <Map
-        style={{width: '50%', height: '50%'}}
+        style={{width: '80%', height: '60%'}}
         google={this.props.google}
         initialCenter={{
             lat: 49.2819605, lng: -123.1086604
@@ -17,8 +37,25 @@ class MapContainer extends Component {
 
         {/* <Marker /> */}
         <Marker
+          onClick={this.onMarkerClick}
           name={'Code Core'}
           position={{lat: 49.2819605, lng: -123.1086604}} />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'NOT Code Core'}
+          position={{lat: 49.2829300, lng: -123.1046604}} />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'NOT Code Core 2'}
+          position={{lat: 49.2529300, lng: -123.1056604}} />
+
+          <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div className= "InfoWindow">
+              <p1>{this.state.selectedPlace.name}</p1>
+            </div>
+        </InfoWindow>
 
         </Map>
       </div>
