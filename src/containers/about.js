15bc1f0585f423ac84816c2a391c93b93b0ee6c2
raymondchen86@hotmail.com <raymondchen86@hotmail.com>
2018-01-29 01:55:57 -0800

    fix nav and footer margin

--- src/containers/about.js
+++ src/containers/about.js
@@ -7,7 +7,12 @@ class About extends React.Component {
   render() {
     return (
       <div className={this.props.aboutOpen ? "about-open" : "about-closed"}>
-        <h3> Eduardo, Anna, Ray, Gloria, Shaggy, Paul </h3>
+        <h3>ABOUT</h3>
+        <p>This project was created by: Eduardo, Anna, Ray, Gloria, Shaggy, Paul.</p>
+
+        <p>Frontend technologies used for this app include React with Redux, and Bootstrap.</p>
+
+          <p>Backend technologies used for this app include Ruby on Rails and Postgres.</p>
       </div>
     )
   }
