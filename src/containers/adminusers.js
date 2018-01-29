db9ad0146889aaaa84828959c2dd2b7e3442dcdc
Eduardo Muramoto <dudmuramoto@gmail.com>
2018-01-28 23:21:55 -0800

    request users and add delete on admin users page

--- src/containers/adminusers.js
+++ src/containers/adminusers.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../actions/index';
+import {User} from '../requests/users';
 
 class AdminUsers extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      allUsers: []
+    };
+  }
+
+  componentDidMount() {
+    User
+      .all()
+      .then (res => (
+        this.setState({
+          allUsers: res
+        })
+      ))
+  }
+
+  deleteUser(userId) {
+    const filteredUsers = this.state.allUsers.filter(user => user.id !== userId)
+    const newState = Object.assign({}, this.state.allUsers, {
+      allUsers: filteredUsers
+    });
+    this.setState(newState);
+    User
+      .destroy(userId)
+  }
+
   render() {
     return (
       <div className={this.props.adminUsersOpen ? "adminusers-open" : "adminusers-closed"}>
         <div className="container">
           <form className='admin-form'>
 
-            <h4 className="admin-title-header">PENDING CLAIM REQUESTS</h4>
+            <h4 className="admin-title-header">USERS</h4>
 
             <div className="container-fluid">
               <div className="row">
@@ -19,12 +47,27 @@ class AdminUsers extends React.Component {
                   <tr className="admin-table-row">
                     <th scope="col-md-12" className="admin-table-head">NAME</th>
                     <th scope="col-md-12" className="admin-table-head">EMAIL</th>
-                    <th scope="col-md-12" className="admin-table-head">ORGANIZATION</th>
+                    {/* <th scope="col-md-12" className="admin-table-head">ORGANIZATION</th> */}
                     <th scope="col-md-12" className="admin-table-head">ACTIONS</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
+                  { this.state.allUsers.map(user => (
+                    <tr key={user.id}>
+                      <th scope="row" className="admin-data">{user.full_name}</th>
+                      <td className="admin-data-remove">{user.email}</td>
+                      {/* <th scope="row" className="admin-data">{user.organization.name}</th> */}
+                      <td className="admin-data-remove">
+                        <button
+                        onClick={()=>this.deleteUser(user.id)}
+                        >
+                          Remove
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                }
+                  {/* <tr>
                     <th scope="row" className="admin-data">JOE SMITH</th>
                     <td className="admin-data-remove">joe@wishpond.com</td>
                     <th scope="row" className="admin-data">Wishpond</th>
@@ -35,7 +78,7 @@ class AdminUsers extends React.Component {
                     <td className="admin-data-remove">joe@wishpond.com</td>
                     <th scope="row" className="admin-data">Wishpond</th>
                     <td className="admin-data-remove">accept reject</td>
-                  </tr>
+                  </tr> */}
 
                 </tbody>
               </table>
