8334dda6311db76884715d632f1b309507369164
Paul Chang <paulchang93@gmail.com>
2018-01-29 04:24:36 -0800

    typo fix in style.scss

--- src/containers/about.js
+++ src/containers/about.js
@@ -10,7 +10,7 @@ class About extends React.Component {
         <h3>ABOUT</h3>
         <p>This project was created by: Eduardo, Anna, Ray, Gloria, Shaggy, Paul.</p>
 
-        <p>Frontend technologies used for this app include React with Redux, and Bootstrap.</p>
+      <p>Frontend technologies used for this app include React with Redux, AJAX and Bootstrap.</p>
 
           <p>Backend technologies used for this app include Ruby on Rails and Postgres.</p>
       </div>
