69e839a1b24636f84a665100c1fa591eff9e06af
Anna Baron <abaron@live.com>
2018-01-28 18:09:07 -0800

    Delete and add functions to admin news page

--- src/containers/adminsearchterms.js
+++ src/containers/adminsearchterms.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import actions from '../actions/index';
-import SearchTerm from '../requests/search_terms';
+import {SearchTerm} from '../requests/search_terms';
 
 class AdminSearchTerms extends React.Component {
   constructor () {
@@ -10,56 +10,88 @@ class AdminSearchTerms extends React.Component {
     this.state = {
       newSearchTerm: {
         title: ""
-      }
+      },
+      allSearchTerms: []
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSubmit(){
+  componentDidMount() {
     SearchTerm
-      .create({title: this.state.newSearchTerm})
-      console.log({title: this.state.newSearchTerm})
+      .all()
+      .then (res => (
+        this.setState({
+          newSearchTerm: {
+            title: ""
+          },
+          allSearchTerms: res
+        })
+      ))
+  }
+
+  handleSubmit(newSearchInfo){
+    const newSearch = this.state.allSearchTerms.slice(0);
+    SearchTerm
+      .create(newSearchInfo)
+      .then(res => {newSearch.push({id: res.id, title: res.title})
+        this.setState(Object.assign({}, this.state, {allSearchTerms: newSearch}));
+    })
   }
 
   handleChange(event) {
     const newState = Object.assign({}, this.state, {
-      [event.target.name]: event.target.value,
+      newSearchTerm: {title: event.target.value}
     });
     this.setState(newState);
   }
 
-  render() {
-    // const terms = SearchTerm.all();
+  deleteSearchTerm(termId) {
+    const filteredTerms = this.state.allSearchTerms.filter(term => term.id !== termId)
+    const newState = Object.assign({}, this.state.allSearchTerms, {
+      allSearchTerms: filteredTerms
+    });
+    this.setState(newState);
+    SearchTerm
+      .destroy(termId)
+  }
 
+  render() {
     return (
       <div className={this.props.adminNewsOpen ? "adminnews-open" : "adminnews-closed"}>
+        <h2>News</h2>
         <form>
           <div className="form-group row">
             <div className="col-sm-12">
-              <input type="text" className="form-control input_title" name="title" onChange={this.handleChange.bind(this)} value={this.state.title}  placeholder="Title"/>
+              <label htmlFor="title">Search Terms</label>
+              <input type="text" className="form-control input_title" name="title" onChange={this.handleChange.bind(this)} value={this.state.newSearchTerm.title}></input>
             </div>
           </div>
-
           <div className="form-group row">
             <div className="col-sm-12 button-column">
-              <button type="button" className="form-submit" onClick={()=>this.handleSubmit()}>SUBMIT</button>
+              <button type="button" className="form-submit" onClick={()=>this.handleSubmit(this.state.newSearchTerm)}>Add</button>
             </div>
           </div>
         </form>
-        {/* <table>
-          <tr><th>Search Term</th><th>Action</th></tr>
-          { terms.map(term => (
-            <tr>
-              <td>{term.title}</td>
-              <td>
-                <button
-                    onClick={this.deleteProduct(term.id)}
-                  >Delete
-                </button>
-              </td>
-            </tr>
+        <p>List of Google News search terms to be fetched daily</p>
+        <table>
+          <thead>
+            <tr><th>Search Term</th><th>Action</th></tr>
+          </thead>
+          <tbody>
+            { this.state.allSearchTerms.map(term => (
+              <tr key={term.id}>
+                <td>{term.title}</td>
+                <td>
+                  <button
+                    onClick={()=>this.deleteSearchTerm(term.id)}
+                    >remove
+                  </button>
+                </td>
+              </tr>
             ))
           }
-        </table> */}
+          </tbody>
+        </table>
       </div>
     )
   }
